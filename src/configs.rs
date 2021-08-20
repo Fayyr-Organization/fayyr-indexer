a9960e702021645a01d25ea37305542ae1ec3aa2
Maria Mihu <mvmihu@gmail.com>
2021-08-20 13:15:34 -0400

    Upgraded to nearcore 1.21.0 and other cleanup

--- src/configs.rs
+++ src/configs.rs
@@ -42,12 +42,20 @@ pub(crate) struct InitConfigArgs {
     /// Genesis file to use when initialize testnet (including downloading)
     #[clap(short, long)]
     pub genesis: Option<String>,
-    #[clap(short, long)]
+    #[clap(long)]
     /// Download the verified NEAR genesis file automatically.
-    pub download: bool,
+    pub download_genesis: bool,
     /// Specify a custom download URL for the genesis-file.
     #[clap(long)]
     pub download_genesis_url: Option<String>,
+    #[clap(long)]
+    /// Download the verified NEAR config file automatically.
+    pub download_config: bool,
+    /// Specify the boot nodes to bootstrap the network
+    pub boot_nodes: Option<String>,
+    /// Specify a custom download URL for the config file.
+    #[clap(long)]
+    pub download_config_url: Option<String>,
     #[clap(long)]
     pub max_gas_burnt_view: Option<u64>,
 }
@@ -71,7 +79,10 @@ impl From<InitConfigArgs> for near_indexer::InitConfigArgs {
             num_shards: config_args.num_shards,
             fast: config_args.fast,
             genesis: config_args.genesis,
-            download: config_args.download,
+            download_config: config_args.download_config,
+            download_genesis: config_args.download_genesis,
+            boot_nodes: config_args.boot_nodes,
+            download_config_url: config_args.download_config_url,
             download_genesis_url: config_args.download_genesis_url,
             max_gas_burnt_view: config_args.max_gas_burnt_view,
         }
